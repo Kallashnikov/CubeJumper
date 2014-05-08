7f79d64e50d4d4fbe1708c69de7af32d79ae3638
jtbrown3 <jtbrown3@RS-F251-S05.tulsatech.org>
2014-05-08 10:17:33 -0500

    ...

--- cubejumper/src/com/me/cubejumper/bases/BaseLevel.java
+++ cubejumper/src/com/me/cubejumper/bases/BaseLevel.java
@@ -34,71 +34,71 @@ public class BaseLevel implements Screen
 	protected static final float TIMESTEP = 1 / 60f;
 	protected static final int VELOCITYIT = 8;
 	protected static final int POSITIONIT = 3;
-	
+
 	public int width, height;
 	public static float startTime, endTime;
 	public static float highScore = 0;
 	public static boolean isSlowMotion = false;
 	public static boolean isSuperJump = false;
-	
+
 	protected static CubeJumper game;
-	
+
 	private Player player;
 	private ContactHandler conHandler;
 	private InputHandler inputHandler;
-	
+
 	protected World world;
 	private Box2DDebugRenderer debugRenderer;
 	protected OrthographicCamera camera;
 	public ShapeRenderer shapeRender;
-	
+
 	private Body groundBody;
 	private BodyDef groundBodyDef;
 	private PolygonShape groundBox;
-	
+
 	@Override
 	public void show() {
 		width = Gdx.graphics.getWidth() / 5;
 		height = Gdx.graphics.getHeight() / 5;
-		
+
 		isSlowMotion = false;
 		isSuperJump = false;
 		BasePowerUp.count = 0;
-		
+
 		camera = new OrthographicCamera(width, height);
-		
+
 		world = new World(new Vector2(15f, -100f), true);
 		debugRenderer = new Box2DDebugRenderer();
 		conHandler = new ContactHandler(game, world);
-		
+
 		player = new Player(world);
 		inputHandler = new InputHandler(world, player);
 		Gdx.input.setInputProcessor(inputHandler);
-		
+
 		world.setContactListener(conHandler);
-		
+
 		startTime = System.nanoTime();
-		
+
 		groundBodyDef = new BodyDef();
 		groundBodyDef.position.set(0, 1);
-		
+
 		groundBody = world.createBody(groundBodyDef);
 		groundBody.setUserData(0);
-		
+
 		groundBox = new PolygonShape();
 		groundBox.setAsBox((camera.viewportWidth) * 100, .5f);
-		
+
 		groundBody.createFixture(groundBox, 0.0f);
 	}
-	
+
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		player.update(camera, delta);
 		camera.update();
-		
+
 		debugRenderer.render(world, camera.combined);
 	}
 
